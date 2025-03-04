8c9d2a660b05791b0edf1a19576160b5fd34956e
Norman Teo <normantch04@gmail.com>
2025-03-05 01:25:30 +0800

    created two contracts and tested with subgraph

--- packages/snake-graph-scroll/src/snake-factory.ts
+++ packages/snake-graph-scroll/src/snake-factory.ts
@@ -1,19 +1,22 @@
-import { GameCreated as GameCreatedEvent } from "../generated/SnakeFactory/SnakeFactory"
-import { GameCreated } from "../generated/schema"
+import { DataSourceTemplate } from "@graphprotocol/graph-ts";
+import { GameCreated as GameCreatedEvent } from "../generated/SnakeFactory/SnakeFactory";
+import { GameCreated } from "../generated/schema";
 
 export function handleGameCreated(event: GameCreatedEvent): void {
+  DataSourceTemplate.create("LobbyGame", [event.params.gameAddress.toHex()]);
+
   let entity = new GameCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameAddress = event.params.gameAddress
-  entity.creator = event.params.creator
-  entity.stakeAmount = event.params.stakeAmount
-  entity.maxPlayers = event.params.maxPlayers
-  entity.duration = event.params.duration
+  );
+  entity.gameAddress = event.params.gameAddress;
+  entity.creator = event.params.creator;
+  entity.stakeAmount = event.params.stakeAmount;
+  entity.maxPlayers = event.params.maxPlayers;
+  entity.duration = event.params.duration;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
